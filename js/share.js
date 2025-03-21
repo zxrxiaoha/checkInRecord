cd5f1e7bfaabe230873e921834c1c0982f749d0f
xiaoha <zhengxiaorong@hxjiot.com>
2025-03-21 15:18:14 +0800

    --

--- js/share.js
+++ js/share.js
@@ -141,6 +141,7 @@ class ShareManager {
             border-radius: 10px;
             box-shadow: 0 0 20px rgba(0,0,0,0.2);
             z-index: 1000;
+            width:80%;
         `;
 
         const img = document.createElement('img');
